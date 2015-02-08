8a40dcf71f081dc9a0026035440ce286fb384cb0
sajaddp <cayber.p@gmail.com>
2015-02-08 15:50:43 +0330

    1

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 $f3 = require('lib/base.php');
+$f3->config('config.ini');
+
 $f3->route('GET /',
     function() {
-        echo 'Hello, world!';
+		$view= new View();
+		echo $view->render('home.php');
     }
 );
-
 $f3->run();
 ?>
