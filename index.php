ec602fb80c2205b576d4155fc46883a4eb1e4c9d
sajaddp <cayber.p@gmail.com>
2015-02-12 23:09:51 +0330

    0.0.931123-sdp
    YES!

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $f3 = require('lib/base.php');
-$f3->set('AUTOLOAD','app/cls/');
+//$f3->set('AUTOLOAD','app/cls/');
 $f3->config('app/config.ini');
 $f3->config('app/routes.ini');
 $f3->run();
